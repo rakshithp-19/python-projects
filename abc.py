f48b02f2ac222f864519390f216fcf51fb73cbb7
Rakshith Patil <rakshithp19@gmail.com>
2018-08-24 19:23:37 +0530

    init

---
+++ abc.py
@@ -0,0 +1,8 @@
+t1= int(input("enter test1 marks: "))
+t2= int(input("enter test2 marks: "))
+t3= int(input("enter test3 marks: "))
+max_1= max(t1,t2)
+max_2= max(t2,t3)
+
+result = (max_1 + max_2 )/2
+print(result)

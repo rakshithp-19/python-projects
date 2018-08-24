1b17231d7b2f9adf133c316ce4459c2310597c88
Rakshith Patil <rakshithp19@gmail.com>
2018-08-24 19:36:07 +0530

    init

---
+++ guessing_game.py
@@ -0,0 +1,20 @@
+secret_word = "Whatsapp"
+guess = ""
+guess_count = 0
+guess_limit = 4
+out_of_guesses = False
+
+while guess != secret_word and not(out_of_guesses):
+     if guess_count < guess_limit:
+            guess = input("enter your guess: ")
+            guess_count += 1
+            if guess!="Whatsapp":
+                print("Wrong answer")
+
+     else:
+        out_of_guesses = True
+
+if out_of_guesses:
+    print("out of guesses you lose")
+else:
+    print("you win")
